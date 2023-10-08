5735639dca0ceddf6f0ec6dc21a172c3a06ff531
Moiz D <133369074+TheB9@users.noreply.github.com>
2023-10-08 11:02:40 -0500

    html_3

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 
-from pymongo.mongo_client import MongoClient
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 
@@ -9,17 +8,6 @@ def hello():
     return render_template("index2.html")
 
 
-uri = "mongodb+srv://hackuta:rpe5xx3YOUWLXFOc@cluster-01.65uvf4f.mongodb.net/?retryWrites=true&w=majority"
-
-# Create a new client and connect to the server
-client = MongoClient(uri)
-
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
 
 
 if __name__ == "__main__":
